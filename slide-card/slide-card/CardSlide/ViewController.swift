e209376cffb48bba2d8edf3a5f59de5611ab0286
inaeLee <inae9709@hanmail.net>
2021-06-27 00:07:08 +0900

    add: setConstraint 함수 추가 (label, collectionView)

--- slide-card/slide-card/CardSlide/ViewController.swift
+++ slide-card/slide-card/CardSlide/ViewController.swift
@@ -12,12 +12,37 @@ class ViewController: UIViewController {
 
     private let label: UILabel = {
         let label = UILabel()
-        label.text = "웅앵우앵"
+        label.text = "📬 Card Silde!"
 
         return label
     }()
 
+    private lazy var collectionViewLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: view.frame.width * 0.8, height: view.frame.height * 0.8)
+
+        return layout
+    }()
+
+    private lazy var
+        collectionView: UICollectionView = {
+            let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+            collectionView.contentInsetAdjustmentBehavior = .never
+            collectionView.decelerationRate = .fast
+            collectionView.backgroundColor = .black
+
+            collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
+            collectionView.delegate = self
+            collectionView.dataSource = self
+
+            return collectionView
+        }()
+
     // MARK: - Properties
+
+    let cardSize = CGSize(width: 300, height: 500)
 
     // MARK: - Initializer
 
@@ -35,7 +60,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setConstraint()
     }
 
@@ -45,14 +70,32 @@ class ViewController: UIViewController {
 
     func setConstraint() {
         label.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
 
-        [label].forEach { v in
+        [label, collectionView].forEach { v in
             view.addSubview(v)
         }
 
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
-        label.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+
+        collectionView.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
 
     // MARK: - Protocols
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
 }

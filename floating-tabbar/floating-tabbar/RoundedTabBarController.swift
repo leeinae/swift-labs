542860c0868919720bd28f7d84d9e33cdb82387b
inaeLee <inae9709@hanmail.net>
2021-06-28 22:24:55 +0900

    add: notch없는 기기에서도 floating.. 그러나 아이콘은 제자리.

--- floating-tabbar/floating-tabbar/RoundedTabBarController.swift
+++ floating-tabbar/floating-tabbar/RoundedTabBarController.swift
@@ -10,13 +10,15 @@ import UIKit
 class RoundedTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         UITabBar.appearance().backgroundColor = .clear
         UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().clipsToBounds = false
         tabBar.backgroundImage = UIImage()
+        tabBar.clipsToBounds = false
 
         let layer = CAShapeLayer()
-        layer.path = UIBezierPath(roundedRect: CGRect(x: 30, y: tabBar.bounds.minY + 5, width: tabBar.bounds.width - 60, height: tabBar.bounds.height + 10), cornerRadius: tabBar.frame.width / 2).cgPath
+        layer.path = UIBezierPath(roundedRect: CGRect(x: 30, y: tabBar.bounds.minY, width: tabBar.bounds.width - 60, height: tabBar.bounds.height + 10), cornerRadius: tabBar.frame.width / 2).cgPath
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
         layer.shadowRadius = 25.0
@@ -31,11 +33,20 @@ class RoundedTabBarController: UITabBarController {
 
         if let items = tabBar.items {
             items.forEach { item in
-                item.imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                item.image = UIImage(named: "btn")
+//                item.imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
+//                item.image?.stretchableImage(withLeftCapWidth: 0, topCapHeight: 48)
             }
         }
 
-        tabBar.itemWidth = 60.0
+        tabBar.itemWidth = 48.0
+        tabBar.itemSpacing = 24
         tabBar.itemPositioning = .centered
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tabBar.frame.size.height = 90
+        tabBar.frame.origin.y = view.frame.height - 90
     }
 }

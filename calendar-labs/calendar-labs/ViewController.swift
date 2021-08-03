0bb8c76d6be0be1072f8e5be4bd13842da881505
inaeLee <inae9709@hanmail.net>
2021-08-04 01:51:23 +0900

    feat: appearance 조정해보기

--- calendar-labs/calendar-labs/ViewController.swift
+++ calendar-labs/calendar-labs/ViewController.swift
@@ -15,7 +15,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setCalendar()
         setConstraints()
+    }
+
+    func setCalendar() {
+        calendar.dataSource = self
+        calendar.delegate = self
+
+        /// 요일 한글 변환
+        calendar.locale = Locale(identifier: "ko_KR ")
+
+        /// 날짜 선택
+        calendar.allowsMultipleSelection = true
+        calendar.allowsSelection = true
+
+        /// textColor
+        calendar.appearance.weekdayTextColor = .black
+        calendar.appearance.todayColor = .purple
+        calendar.appearance.headerTitleColor = .black
+
+        /// hide top, bottom border
+        calendar.clipsToBounds = true
+
+//        calendar.today = nil /// 오늘 표시 숨기기
+
+        /// Header
+        calendar.appearance.headerTitleFont = UIFont.systemFont(ofSize: 25)
+        calendar.appearance.headerDateFormat = "M월 YYYY년"
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
     }
 
     func setConstraints() {
@@ -23,8 +51,21 @@ class ViewController: UIViewController {
 
         calendar.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(100)
-            make.leading.trailing.equalToSuperview().inset(50)
-            make.bottom.equalToSuperview().offset(-100)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(300)
         }
+    }
+}
+
+/// 날짜 아래 이미지 띄우기
+extension ViewController: FSCalendarDataSource {
+//    func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
+//        UIImage(named: "habbit5")
+//    }
+}
+
+extension ViewController: FSCalendarDelegateAppearance {
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, imageOffsetFor date: Date) -> CGPoint {
+        CGPoint(x: .zero, y: -5)
     }
 }

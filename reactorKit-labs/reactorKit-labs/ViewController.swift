7ecaa8db54a062a5a79f005e0672490485579bfc
Inae Lee <inae9709@hanmail.net>
2022-07-09 01:30:06 +0900

    feat: follow button reactor 구현

--- reactorKit-labs/reactorKit-labs/ViewController.swift
+++ reactorKit-labs/reactorKit-labs/ViewController.swift
@@ -5,25 +5,42 @@
 //  Created by Devsisters on 2022/07/08.
 //
 
+import ReactorKit
+import RxCocoa
 import RxSwift
 import SnapKit
 import UIKit
 
 /// https://medium.com/styleshare/reactorkit-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-c7b52fbb131a
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, View {
+    typealias Reactor = ViewReactor
+
     // MARK: - UI Components
 
     lazy var followButton: UIButton = {
         let button = UIButton()
         button.configuration = followButtonConfiguration
+        button.configurationUpdateHandler = followAction
 
         return button
     }()
 
+    let followAction: UIButton.ConfigurationUpdateHandler = { button in
+        switch button.state {
+            case .normal:
+                button.configuration?.title = "Follow Button"
+            case .selected:
+                button.configuration?.title = "Unfollow Button"
+            default: break
+        }
+    }
+
     // MARK: - Properties
 
-    private let disposeBag = DisposeBag()
+    var disposeBag: DisposeBag = .init()
+    private let reactor = Reactor()
+
     private var followButtonConfiguration = UIButton.Configuration.filled()
 
     // MARK: - Life Cycle Methods
@@ -32,16 +49,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         setupUI()
+        bind(reactor: reactor)
     }
 
     // MARK: - Configure Methods
 
     func setupUI() {
         view.backgroundColor = .white
-        
-        followButtonConfiguration.baseBackgroundColor = .systemBlue
 
-        var titleAttr = AttributedString("Follow Button")
+        var titleAttr = AttributedString()
         titleAttr.font = .systemFont(ofSize: 26.0, weight: .heavy)
         followButtonConfiguration.attributedTitle = titleAttr
 
@@ -52,5 +68,20 @@ class ViewController: UIViewController {
         }
     }
 
-    func bind() {}
+    // MARK: - Bind Method
+
+    func bind(reactor: ViewReactor) {
+        /// input
+        followButton.rx.tap
+            .scan(false) { lastState, _ in !lastState }
+            .map { $0 ? .follow : .unfollow }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        /// output
+        reactor.state.map { $0.isFollowing }
+            .distinctUntilChanged()
+            .bind(to: followButton.rx.isSelected)
+            .disposed(by: disposeBag)
+    }
 }
--- reactorKit-labs/reactorKit-labs/ViewReactor.swift
+++ reactorKit-labs/reactorKit-labs/ViewReactor.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import ReactorKit
+
+final class ViewReactor: Reactor {
+    /// user interaction
+    enum Action {
+        case follow
+        case unfollow
+    }
+
+    /// view state
+    struct State {
+        var isFollowing: Bool
+    }
+
+    /// changes view state
+    /// class 내부에서 Action - State를 연결하는 역할
+    enum Mutation {
+        case setFollowing(Bool)
+    }
+
+    let initialState: State
+
+    init() {
+        self.initialState = State(isFollowing: false)
+    }
+}
+
+extension ViewReactor {
+    /// actionStream to mutationStream (call reduce)
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            case .follow:
+                /// service logic - follow
+                return Observable.just(Mutation.setFollowing(true))
+            case .unfollow: 
+                /// service logic - unfollow
+                return Observable.just(Mutation.setFollowing(false))
+        }
+    }
+
+    /// mutationStream, return to State
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+
+        switch mutation {
+            case let .setFollowing(isfollowing):
+                newState.isFollowing = isfollowing
+        }
+
+        return newState
+    }
+}

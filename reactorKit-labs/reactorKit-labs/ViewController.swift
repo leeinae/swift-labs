7e34bb36ab14977a73965b079fb2105fefeb5efe
Inae Lee <inae9709@hanmail.net>
2022-07-08 21:13:40 +0900

    init: ReactorKit 실험 시작 ㅋㅋ

---
+++ reactorKit-labs/reactorKit-labs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reactorKit-labs
+//
+//  Created by Devsisters on 2022/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+
+    func setupUI() {
+        view.backgroundColor = .white
+    }
+}
---
+++ reactorKit-labs/reactorKit-labsUITests/reactorKit_labsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  reactorKit_labsUITestsLaunchTests.swift
+//  reactorKit-labsUITests
+//
+//  Created by Devsisters on 2022/07/08.
+//
+
+import XCTest
+
+class reactorKit_labsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

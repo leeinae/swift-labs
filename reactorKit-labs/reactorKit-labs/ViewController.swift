f5bdea9b74bf94101828b315cfcfb439a85f7cf7
Inae Lee <inae9709@hanmail.net>
2022-07-08 21:26:38 +0900

    add: 기본 뷰 구성

--- reactorKit-labs/reactorKit-labs/ViewController.swift
+++ reactorKit-labs/reactorKit-labs/ViewController.swift
@@ -5,16 +5,52 @@
 //  Created by Devsisters on 2022/07/08.
 //
 
+import RxSwift
+import SnapKit
 import UIKit
 
+/// https://medium.com/styleshare/reactorkit-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0-c7b52fbb131a
+
 class ViewController: UIViewController {
+    // MARK: - UI Components
+
+    lazy var followButton: UIButton = {
+        let button = UIButton()
+        button.configuration = followButtonConfiguration
+
+        return button
+    }()
+
+    // MARK: - Properties
+
+    private let disposeBag = DisposeBag()
+    private var followButtonConfiguration = UIButton.Configuration.filled()
+
+    // MARK: - Life Cycle Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
     }
 
+    // MARK: - Configure Methods
+
     func setupUI() {
         view.backgroundColor = .white
+        
+        followButtonConfiguration.baseBackgroundColor = .systemBlue
+
+        var titleAttr = AttributedString("Follow Button")
+        titleAttr.font = .systemFont(ofSize: 26.0, weight: .heavy)
+        followButtonConfiguration.attributedTitle = titleAttr
+
+        view.addSubview(followButton)
+
+        followButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
+
+    func bind() {}
 }
---
+++ reactorKit-labs/reactorKit-labs/ViewReactor.swift
@@ -0,0 +1,8 @@
+//
+//  ViewReactor.swift
+//  reactorKit-labs
+//
+//  Created by Devsisters on 2022/07/08.
+//
+
+import Foundation

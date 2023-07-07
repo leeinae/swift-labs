bbb49b329fce0ab09ba96d169e742a5fa2cded0f
inae <vvithin.two@gmail.com>
2023-07-07 23:00:30 +0900

    macro의 향기만 조금 느껴보다 ..

---
+++ LabMacro/Sources/LabMacroClient/main.swift
@@ -0,0 +1,8 @@
+import LabMacro
+
+let a = 17
+let b = 25
+
+let (result, code) = #stringify(a + b)
+
+print("The value \(result) was produced by the code \"\(code)\"")
---
+++ LabMacro/Sources/LabMacroMacros/Plugin.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//
+//
+//  Created by Inae Lee on 2023/07/04.
+//
+
+import Foundation
+import SwiftCompilerPlugin
+import SwiftSyntaxMacros
